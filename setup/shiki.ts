72d9c76cd62503e3aff0c574f6005bf2c0e8cca0
alvarosabu <alvaro.saburido@gmail.com>
2024-03-23 10:33:38 +0100

    chore: remove console.log

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -4,7 +4,6 @@ import fs from 'node:fs'
 import { defineShikiSetup } from '@slidev/types'
 
 const myTheme = JSON.parse(fs.readFileSync('setup/theme/penguin-theme.json', 'utf8'))
-console.log(myTheme)
 
 export default defineShikiSetup(() => ({
   theme: myTheme,
