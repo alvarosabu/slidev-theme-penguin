51dcc069334b8b2610f3931959dec12da8d5592f
alvarosabu <alvaro.saburido@gmail.com>
2024-03-23 10:11:29 +0100

    feat: updated to new slidev version

--- setup/shiki.ts
+++ setup/shiki.ts
@@ -1,6 +1,11 @@
 /* ./setup/shiki.ts */
+import fs from 'node:fs'
+
 import { defineShikiSetup } from '@slidev/types'
 
+const myTheme = JSON.parse(fs.readFileSync('setup/theme/penguin-theme.json', 'utf8'))
+console.log(myTheme)
+
 export default defineShikiSetup(() => ({
-  theme: 'css-variables',
+  theme: myTheme,
 }))
